59237baa4f73bb8300456f10e85147245062f58d
NazarKotsun <91723135+NazarKotsun@users.noreply.github.com>
2023-06-30 14:53:03 +0300

    UC#7 Infrastructure setup

---
+++ UC7/UC7.UnitTests/StudentConverterTests.cs
@@ -0,0 +1,10 @@
+namespace UC7.UnitTests
+{
+    public class StudentConverterTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ UC7/UC7/Services/StudentConverter.cs
@@ -0,0 +1,45 @@
+﻿using US7.Models;
+
+public class StudentConverter
+{
+    public List<Student> ConvertStudents(List<Student> students)
+    {
+        return students.Select(student =>
+        {
+            var result = new Student
+            {
+                Name = student.Name,
+                Age = student.Age,
+                Grade = student.Grade
+
+            };
+
+            if (student.Grade > 90)
+            {
+                if (student.Age < 21)
+                {
+                    result.Exceptional = true;
+                }
+
+                else
+                {
+                    result.HonorRoll = true;
+                }
+
+            }
+
+            else if (student.Grade > 70)
+            {
+                result.Passed = true;
+            }
+
+            else
+            {
+                result.Passed = false;
+            }
+
+            return result;
+
+        }).ToList();
+    }
+}
